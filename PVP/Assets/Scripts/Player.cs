44c1620bc25a27d86c4fba6cdb07dd1856ceefd3
Ruofan Wu <rwu17@student.aau.dk>
2018-10-30 08:10:00 +0100

    Begin

---
+++ PVP/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    public GameObject playerSphere;
+
+    void Update()
+    {
+        if(isLocalPlayer == true)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                this.transform.Translate(Vector3.right * Time.deltaTime * 3f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                CmdSpawn();
+            }
+        }
+    }
+
+    [Command]
+    public void CmdSpawn()
+    {
+        GameObject sphere = (GameObject)Instantiate(playerSphere, transform.position, Quaternion.identity);
+        NetworkServer.SpawnWithClientAuthority(sphere, connectionToClient);
+    }
+
+}
---
+++ PVP/Assets/Scripts/PlayerSphere.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSphere : NetworkBehaviour {
+    void Update()
+    {
+        if (hasAuthority == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.transform.Translate(Vector3.up * Time.deltaTime * 3f);
+            }
+        }
+    }
+}
