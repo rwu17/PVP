4f77e71d66d2a4879ecfb9d518085dd60edddd4a
Ruofan Wu <rwu17@student.aau.dk>
2018-11-01 00:33:07 +0100

    Animation Begin

---
+++ PVP/Assets/Scripts/IdleToRun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleToRun : MonoBehaviour {
+
+    public Animator animator;
+    
+
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetInteger("Condition", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetInteger("Condition", 0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
